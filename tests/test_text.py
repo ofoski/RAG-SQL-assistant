8ec9397b7ddcdd2b76b563e07631d61cc7e7768a
Ali Hosein Ghanemi <61706448+ofoski@users.noreply.github.com>
2025-09-02 16:35:14 -0400

    Update test_text.py

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,2 +1,9 @@
-def test_text():
-    assert True
+from load_data import get_metadata, run_query
+
+def test_simple_count_query():
+    """Run a basic query on the Chinook DB"""
+    df = run_query("SELECT COUNT(*) as n FROM Customer;")
+    assert "n" in df.columns
+    assert df.shape[0] == 1
+
+
